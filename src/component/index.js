77d4d2ca868743dd87d5e431ef044e3ea652b84e
hyeoz <jenny970203@gmail.com>
2023-01-05 20:04:12 +0900

    publish error

--- src/component/index.js
+++
@@ -1,7 +0,0 @@
-import { Button } from "../stories/Button";
-import { Input } from "../stories/Common/Input";
-import { TextButton } from "../stories/Common/TextButton";
-import Footer from "../stories/Common/Footer";
-import Icons from "../stories/Common/icons";
-
-export { Button, Input, TextButton, Footer, Icons };
