f9f2738baeba492a4f3e3581081075ba53d9ae5c
hyeoz <jenny970203@gmail.com>
2023-01-05 18:19:50 +0900

    first publish to npm

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MAYDAY DESIGN SYSTEM
 
+메이데이 파트너스 프론트엔드 개발팀의 디자인 시스템입니다!
+
 ## 스크립트 실행
 
 ### `npm run storybook`
---
+++ src/component/index.js
@@ -0,0 +1,7 @@
+import { Button } from "../stories/Button";
+import { Input } from "../stories/Common/Input";
+import { TextButton } from "../stories/Common/TextButton";
+import Footer from "../stories/Common/Footer";
+import Icons from "../stories/Common/icons";
+
+export { Button, Input, TextButton, Footer, Icons };
