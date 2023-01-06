20e9573f807cac46e116eb4758efb62c2bb5c245
hyeoz <jenny970203@gmail.com>
2023-01-06 17:54:44 +0900

    feat: checkbox, radio component

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Button } from "./Button";
 import { Input } from "./Input";
 import { TextButton } from "./TextButton";
 import * as Footer from "./Footer";
+import { Checkbox } from "./checkbox";
 
-export { Button, Input, TextButton, Footer };
+export { Button, Input, TextButton, Footer, Checkbox };
