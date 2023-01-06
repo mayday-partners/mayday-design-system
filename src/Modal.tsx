1906aca2daa35e8dcfc01ad4d7926077e107f83e
leeellen <lee.ellen0814@gmail.com>
2023-01-06 17:34:11 +0900

    design : 웹, 모바일 modal ui 구현

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
 import { Button, ConfigProvider, Modal as AntModal, ModalProps } from 'antd';
-import style from './styles/modal.module.css';
+import './styles/modal.css';
 
 export type ModalType = {
-    type: 'a' | 'b' | 'c';
+    type: 'web' | 'mobile';
+    buttonType: 'one' | 'two';
     colorPrimary?: string;
 } & ModalProps;
 
 export default function Modal({
+    type,
+    buttonType,
     colorPrimary = '#FB5C30',
-    children,
-    okText,
-    cancelText,
+    children = 'helloworld',
+    okText = '확인',
+    cancelText = '취소',
     onOk,
     onCancel,
     ...props
 }: ModalType) {
+    const size = type === 'web' ? 'large' : 'middle';
+
     return (
         <ConfigProvider
             theme={{
@@ -24,15 +28,33 @@ export default function Modal({
                 },
             }}
         >
-            <div>
-                <AntModal closeIcon={<></>} closable={false} {...props} footer={<></>}>
+            <AntModal className={`${type}-modal`} closeIcon={<></>} closable={false} footer={<></>} {...props}>
+                <div style={{ textAlign: 'center' }}>
                     {children}
-                    <div className={`${style.footer}`}>
-                        <Button>{cancelText}</Button>
-                        <Button type="primary">{okText}</Button>
+
+                    <div style={{ marginTop: 24 }}>
+                        {buttonType === 'one' && (
+                            // @ts-ignore
+                            <Button type="primary" size={size} onClick={onOk}>
+                                {okText}
+                            </Button>
+                        )}
+
+                        {buttonType === 'two' && (
+                            <div className="modal-footer">
+                                {/* @ts-ignore */}
+                                <Button size={size} onClick={onCancel}>
+                                    {cancelText}
+                                </Button>
+                                {/* @ts-ignore */}
+                                <Button type="primary" size={size} onClick={onOk}>
+                                    {okText}
+                                </Button>
+                            </div>
+                        )}
                     </div>
-                </AntModal>
-            </div>
+                </div>
+            </AntModal>
         </ConfigProvider>
     );
 }
--- src/modal.stories.tsx
+++ src/modal.stories.tsx
@@ -4,12 +4,21 @@ import Modal, { ModalType } from './Modal';
 export default {
     title: 'Common/Modal',
     component: Modal,
+    argTypes: {
+        colorPrimary: {
+            description: '테마 컬러',
+            control: { type: 'color' },
+        },
+    },
 };
 
 const Template: Story<ModalType> = (args) => <Modal {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    type: 'a',
+    type: 'web',
+    buttonType: 'two',
     open: true,
+    onOk: () => alert('ok'),
+    onCancel: () => alert('cancel'),
 };
