16d8121dcc7c5b2682d9889fe8c463f758f44b76
hyeoz <jenny970203@gmail.com>
2023-01-09 10:22:36 +0900

    fix: rollup css error fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 // import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-// import postcss from "rollup-plugin-postcss";
+import postcss from "rollup-plugin-postcss";
 // import { uglify } from "rollup-plugin-uglify";
 // import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
@@ -18,9 +18,9 @@ export default {
     // peerDepsExternal(),
     resolve(),
     typescript(),
-    // postcss({
-    //   extensions: [".css"],
-    // }),
+    postcss({
+      extensions: [".css"],
+    }),
     // uglify(),
     // babel(),
     commonjs(),
