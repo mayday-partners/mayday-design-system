210ec30706855e1f2877fbe1cf5d6fe8088540cf
hyeoz <jenny970203@gmail.com>
2023-01-09 10:13:36 +0900

    fix: export fix

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Input } from "./Input";
 import { TextButton } from "./TextButton";
 import * as Footer from "./Footer";
 import { Checkbox } from "./Checkbox";
+import Modal from "./Modal";
 
-export { Button, Input, TextButton, Footer, Checkbox };
+export { Button, Input, TextButton, Footer, Checkbox, Modal };
