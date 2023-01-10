1309ade33c1c648adc81bc5436c497b6e21e317d
hyeoz <jenny970203@gmail.com>
2023-01-10 10:52:00 +0900

    feat: select box 컴포넌트 추가

---
+++ src/SelectBox.tsx
@@ -0,0 +1,66 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { ReactElement } from "react";
+import { Select, SelectProps } from "antd";
+import styled from "@emotion/styled";
+import { SerializedStyles } from "@emotion/utils";
+
+import palette from "./styles/palette";
+import Label from "./Label";
+
+export type SelectBoxProps = {
+  items: { value: string; label: string }[];
+  label?: string | ReactElement;
+} & { css?: SerializedStyles } & SelectProps;
+
+/**
+ * SELECTBOX 컴포넌트
+ * @param items select 할 요소 리스트
+ * @param label box 위에 라벨이 있는 경우
+ * @param css
+ * @param 그 외 select props (value, onChange ...)
+ * @returns
+ */
+export default function SelectBox({
+  items,
+  css,
+  label,
+  ...props
+}: SelectBoxProps) {
+  return (
+    <>
+      {label && (
+        <Label type="body1" bold="medium" color={palette.gray[600]}>
+          {label}
+        </Label>
+      )}
+      <SelectDiv css={css}>
+        <Select {...props} options={items} />
+      </SelectDiv>
+    </>
+  );
+}
+
+const SelectDiv = styled.div`
+  display: flex;
+  height: 40px;
+  border: 1px solid ${palette.gray[300]};
+  border-radius: 2px;
+  align-items: center;
+
+  .ant-select {
+    width: 100%;
+
+    .ant-select-selector {
+      border: none;
+      box-shadow: none !important;
+
+      font-weight: 500;
+      font-size: 15px;
+      line-height: 19px;
+    }
+    &::placeholder {
+      color: ${palette.gray[400]};
+    }
+  }
+`;
---
+++ src/stories/SelectBox.stories.tsx
@@ -0,0 +1,43 @@
+import { css } from "@emotion/react";
+import { Story } from "@storybook/react";
+import React from "react";
+import SelectBox, { SelectBoxProps } from "../SelectBox";
+
+var value = "";
+
+export default {
+  title: "Common/SelectBox",
+  component: SelectBox,
+  argTypes: {},
+};
+
+const Template: Story<SelectBoxProps> = (args) => <SelectBox {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  //   onChange: (value) => console.log(value),
+  placeholder: "placeholder",
+  items: [
+    {
+      value: "1",
+      label: "1st menu item",
+    },
+    {
+      value: "2",
+      label: "2st menu item",
+    },
+    {
+      value: "3",
+      label: "3st menu item",
+    },
+    {
+      value: "4",
+      label: "4st menu item",
+    },
+  ],
+  onChange: (v) => {
+    value = v;
+    console.log(value);
+  },
+  label: "",
+};
