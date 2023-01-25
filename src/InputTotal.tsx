806a4fd2dac732d5f53610c0df8c93c56db3ed14
hyeoz <jenny970203@gmail.com>
2023-01-25 13:49:43 +0900

    feat: compound component pattern Input 수정

--- src/InputTotal.tsx
+++ src/InputTotal.tsx
@@ -1,55 +1,29 @@
 import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
 import generatePicker from "antd/es/date-picker/generatePicker";
 import { Dayjs } from "dayjs";
-import { ReactElement } from "react";
+import { HTMLAttributes, KeyboardEvent, ReactElement } from "react";
 import { SerializedStyles } from "@emotion/react";
-import { DatePickerProps, InputProps, TimePickerProps } from "antd";
+import { DatePickerProps, Input, InputProps, TimePickerProps } from "antd";
 import Label from "./Label";
 import palette from "./styles/palette";
 import Icons from "./icons";
+import "./styles/input.css";
 
 const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
-type BaseType = {
-  //   type?:
-  //     | "default"
-  //     | "option"
-  //     | "search"
-  //     | "date"
-  //     | "datetime"
-  //     | "time"
-  //     | "dropbox"
-  //     | "file";
-  onChange: (value: any) => void;
-  onPressEnter?: _.DebouncedFunc<() => void> | (() => void);
-  option?: string;
-  label?: string | ReactElement; // 기본 라벨 스타일과 다르다면 라벨 컴포넌트 바로 넣을 수 있도록
-  //   css?: SerializedStyles;
-  value?: any;
-};
 
-type InputDateType = BaseType & Omit<DatePickerProps, "picker" & "value">;
-
-export default function CompoundInput({
+function CompoundInput({
   children,
   css,
+  label,
   ...props
 }: {
   children: ReactElement;
   css?: SerializedStyles;
-}) {
+  label?: string | ReactElement; // 기본 라벨 스타일과 다르다면 라벨 컴포넌트 바로 넣을 수 있도록
+} & HTMLAttributes<HTMLDivElement>) {
   return (
-    <div id="Input-container" css={css} {...props}>
-      {children}
-    </div>
-  );
-}
-CompoundInput.Date = function InputDate({
-  onChange,
-  label,
-  ...restProps
-}: InputDateType) {
-  return (
-    <>
+    <div {...props} id="input-container" css={css}>
+      {/*  */}
       {label && typeof label === "string" ? (
         <Label type="body1" bold="medium" color={palette.gray.gray6}>
           {label}
@@ -57,12 +31,97 @@ CompoundInput.Date = function InputDate({
       ) : (
         label
       )}
+      {children}
+    </div>
+  );
+}
+
+type BaseType = {
+  onChange: (value: any) => void;
+  onPressEnter?: _.DebouncedFunc<() => void> | (() => void);
+  option?: string;
+  value?: any;
+};
+type InputDateType = BaseType & Omit<DatePickerProps, "picker" & "value">;
+
+CompoundInput.Date = function InputDate({
+  onChange,
+  ...restProps
+}: InputDateType) {
+  return (
+    <div className="input-date">
       <DatePicker
         {...restProps}
         placeholder="연도/월/일"
         onChange={onChange}
         suffixIcon={<Icons icon="calendar" />}
       />
-    </>
+    </div>
   );
 };
+
+CompoundInput.DateTime = function InputDateTime({
+  onChange,
+  ...restProps
+}: InputDateType) {
+  const datetimeProps = {
+    showTime: true,
+  };
+  return (
+    <div className="input-date">
+      <DatePicker
+        {...restProps}
+        {...datetimeProps} // ANCHOR showTime 타입에러
+        format={"YYYY/MM/DD HH:mm"}
+        placeholder="연도/월/일 00:00"
+        onChange={onChange}
+        suffixIcon={<Icons icon="calendar" />}
+      />
+    </div>
+  );
+};
+
+type InputTimeType = BaseType & Omit<TimePickerProps, "picker" & "value">;
+
+CompoundInput.Time = function InputTime({
+  onChange,
+  ...restProps
+}: InputTimeType) {
+  return (
+    <div className="input-date">
+      <DatePicker.TimePicker
+        {...restProps}
+        format={"HH:mm"}
+        placeholder="00:00"
+        onChange={onChange}
+        suffixIcon={<Icons icon="clock" />}
+      />
+    </div>
+  );
+};
+
+type InputTextType = BaseType & Omit<InputProps, "value">;
+
+CompoundInput.Text = function InputText({
+  onPressEnter,
+  onChange,
+  ...restProps
+}: InputTextType) {
+  const onKeyDown = (event: KeyboardEvent) => {
+    // lodash 수정
+    if (
+      event.key === "Enter" &&
+      event.nativeEvent.isComposing === false &&
+      onPressEnter
+    ) {
+      onPressEnter();
+    }
+  };
+  return (
+    <div className="input-text">
+      <Input {...restProps} onPressEnter={onKeyDown} onChange={onChange} />
+    </div>
+  );
+};
+
+export default CompoundInput;
--- src/stories/CompoundComponentPattern.stories.tsx
+++ src/stories/CompoundComponentPattern.stories.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
 import { Story } from "@storybook/react";
 
@@ -11,8 +12,31 @@ export default {
 
 const Template: Story<any> = (args) => <CompoundInput {...args} />;
 
-export const CompoundDate = Template.bind({});
-CompoundDate.args = {
+export const Date = Template.bind({});
+Date.args = {
+  label: "test",
   children: <CompoundInput.Date onChange={(value) => console.log(value)} />,
-  css: css``,
+  css: css`
+    /* background-color: red !important; */
+  `,
+};
+
+export const DateTime = Template.bind({});
+DateTime.args = {
+  children: <CompoundInput.DateTime onChange={(value) => console.log(value)} />,
+};
+
+export const Time = Template.bind({});
+Time.args = {
+  children: <CompoundInput.Time onChange={(value) => console.log(value)} />,
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  children: (
+    <CompoundInput.Text
+      onChange={(value) => console.log(value)}
+      placeholder="placeholder"
+    />
+  ),
 };
