6a98a7698fe526256ab24708fd63d1b4f5c49ae6
hyeoz <jenny970203@gmail.com>
2023-01-26 13:53:58 +0900

    fix: icon 컴포넌트 width, height 수정

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -5,10 +5,14 @@ export type IconsType = keyof typeof svg;
 
 type IconsProps = {
   icon: IconsType;
+  width?: number;
+  height?: number;
 } & React.SVGAttributes<SVGSVGElement>;
 
-export default function Icons({ icon, ...props }: IconsProps) {
+export default function Icons({ icon, width, height, ...props }: IconsProps) {
   return React.createElement(svg[icon], {
+    width,
+    height,
     ...props,
   });
 }
