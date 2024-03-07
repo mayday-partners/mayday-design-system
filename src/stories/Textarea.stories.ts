a27da9987c543f660a13ba77aac186f0e3c630aa
hyeoz <jenny970203@gmail.com>
2024-03-07 17:13:15 +0900

    add: textarea 컴포넌트

---
+++ src/stories/Textarea.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Textarea from "./Textarea";
+
+const meta = {
+  title: "Example/Textarea",
+  component: Textarea,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    textareaType: "round",
+    value: "123123",
+    setValue: (value) => {},
+    isResize: false,
+    isError: true,
+    placeholder: "Place Holder",
+    style: {
+      width: 600,
+      height: 200,
+    },
+  },
+};
+export const Disabled: Story = {
+  args: {
+    textareaType: "angulate",
+    value: "",
+    setValue: (value) => {},
+    disabled: true,
+    placeholder: "Place Holder",
+    style: {
+      width: 600,
+      height: 200,
+    },
+  },
+};
