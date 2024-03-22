1ea6071789db64969bb10a1e6bde68a35132d4fa
hyeoz <jenny970203@gmail.com>
2024-03-22 15:32:30 +0900

    feat: 체크박스 컴포넌트 추가

---
+++ src/stories/Checkbox.stories.ts
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Checkbox from "./Checkbox";
+import { css } from "@emotion/react";
+
+const meta: Meta<typeof Checkbox> = {
+  title: "Example/Checkbox",
+  component: Checkbox,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Checked: Story = {
+  args: {
+    options: [
+      {
+        value: "1",
+        label: "1번",
+      },
+      {
+        value: "2",
+        label: "2번",
+      },
+    ],
+    items: ["1"],
+    setItems: () => {},
+    horizontal: true,
+    wrapperCss: css`
+      gap: 8px;
+    `,
+  },
+};
---
+++ src/stories/Checkbox.tsx
@@ -0,0 +1,92 @@
+import { Interpolation, SerializedStyles, Theme, css } from "@emotion/react";
+import {
+  ChangeEvent,
+  ClassAttributes,
+  Dispatch,
+  InputHTMLAttributes,
+  SetStateAction,
+} from "react";
+
+import "../styles/checkbox.css";
+import { OptionType } from "../utils/types";
+import Icons from "../icons";
+import palette from "../styles/palette";
+import useIsDarkMode from "../utils/useIsDarkMode";
+
+type CheckboxPropsType = {
+  options: OptionType[];
+  items: string[];
+  setItems: Dispatch<SetStateAction<string[]>>;
+  horizontal?: boolean;
+  wrapperCss?: SerializedStyles;
+} & ClassAttributes<HTMLInputElement> &
+  InputHTMLAttributes<HTMLInputElement> & {
+    css?: Interpolation<Theme>;
+  };
+
+export default function Checkbox({
+  options,
+  items,
+  setItems,
+  horizontal = false,
+  wrapperCss,
+  ...props
+}: CheckboxPropsType) {
+  const isDarkMode = useIsDarkMode();
+
+  const onChangeEachCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+    if (items?.includes(e.target.value)) {
+      setItems((prev) => [...prev.filter((p) => p !== e.target.value)]);
+    } else {
+      setItems((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  return (
+    <div
+      className="checkbox-wrapper"
+      css={css`
+        flex-direction: ${horizontal ? "row" : "column"};
+
+        ${wrapperCss}
+      `}
+    >
+      {options.map((option) => (
+        <div className="checkbox-row">
+          <input
+            type="checkbox"
+            value={option.value}
+            checked={items.includes(option.value)}
+            id={option.value}
+            onChange={onChangeEachCheckbox}
+            {...props}
+          />
+          <label
+            htmlFor={option.value}
+            className={`checkbox-item ${
+              items.includes(option.value)
+                ? "checkbox-checked"
+                : "checkbox-default"
+            }`}
+          >
+            {items.includes(option.value) ? (
+              <Icons
+                icon="checked"
+                width={16}
+                height={16}
+                color={
+                  isDarkMode
+                    ? palette.textColor.dark.primary
+                    : palette.textColor.light.primary
+                }
+              />
+            ) : (
+              <div></div>
+            )}
+          </label>
+          <label htmlFor={option.value}>{option.label}</label>
+        </div>
+      ))}
+    </div>
+  );
+}
